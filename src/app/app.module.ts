b3bfa7cafb2bf34e9eb9f0b938bca8c5a72eca99
Masha <mashakalachevalove@gmail.com>
2020-07-15 02:45:00 +0400

    Initial comment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SearchComponent } from './search/search.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatTabsModule} from '@angular/material/tabs';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+import {GenreService} from './services/genre.service';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    SearchComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatTabsModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [GenreService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/sidenav/sidenav.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.css']
+})
+export class SidenavComponent implements OnInit {
+
+  genres: Genre[];
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+
+  }
+}
+  interface Genre{
+    id:number,
+    name:string
+  }
